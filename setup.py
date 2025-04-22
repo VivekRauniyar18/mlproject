e0b7bd193b2837e8a2d668f213f29b808c653677
Vivek Rauniyar <vivekrauniyar18@gmail.com>
2025-04-22 08:58:25 -0400

    logging and exception

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name = 'mlproject',
     version = '0.0.1',
-    author = "Krish", 
+    author = "Vivek", 
     author_email = "vivekrauniyar18@gmail.com",
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
